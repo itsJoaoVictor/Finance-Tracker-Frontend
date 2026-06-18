ba35275b547543d92752b94c87afecff314db750
itsJoaoVictor <jvgf98@gmail.com>
2026-06-18 02:52:33 -0300

    fix(contas): remove key={pageTitle} do main que causava remount do Contas e logout 401

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,6 @@ function AppShell({
           onLogout={handleLogout}
         />
         <main
-          key={pageTitle}
           className="sidebar-layout__content app__screen app__screen--enter"
           style={{ padding: '32px' }}
         >
